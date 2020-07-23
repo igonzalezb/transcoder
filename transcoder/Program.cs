ccebd2d4d017ca20c07415f9291eec44d8765b71
igonzalezbig <63425840+igonzalezbig@users.noreply.github.com>
2020-07-23 20:07:05 -0300

    publish settings

--- transcoder/Program.cs
+++ transcoder/Program.cs
@@ -20,6 +20,11 @@ namespace Transcoder
 		{
 			Console.Title = "Transcoder";
 
+			Console.WriteLine("Welcome to Transcoder!");
+			Console.WriteLine();
+			Console.WriteLine("The HEVC settings are:");
+			Console.WriteLine("ffmpeg -vsync 0 -hwaccel cuvid -c:v h264_cuvid -i video -c:v hevc_nvenc -x265-params crf=20 -spatial_aq 1 -rc-lookahead 20 -preset slow -c:a aac -b:a 224k -map 0 video-trans.mkv");
+			Console.WriteLine();
 			Console.WriteLine("Enter path:");
 			string path = Console.ReadLine();
 
@@ -47,20 +52,20 @@ namespace Transcoder
 			{
 				bool recurse = false;
 				string videoFilter = @"$(?<=\.(mkv|mp4|avi|mk3d|flv|wmv|m4v|webm))";
-				string subsFilter = @"$(?<=\.(srt|idx|sub))";
+				//string subsFilter = @"$(?<=\.(srt|idx|sub))";
 
 				var videos = Directory
 					.GetFiles(path, "*.*", recurse ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
 					.Where(x => Regex.IsMatch(x, videoFilter, RegexOptions.IgnoreCase))
 					.ToList();
-				var subtitlesSRT = Directory
-					.GetFiles(path, "*.*", recurse ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
-					.Where(x => Regex.IsMatch(x, subsFilter, RegexOptions.IgnoreCase))
-					.ToList();
+				//var subtitlesSRT = Directory
+				//	.GetFiles(path, "*.*", recurse ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
+				//	.Where(x => Regex.IsMatch(x, subsFilter, RegexOptions.IgnoreCase))
+				//	.ToList();
 
 				foreach (var video in videos)
 				{
-					Console.WriteLine(Path.GetFileName(video));
+					Console.WriteLine(Path.GetFileName(video) + " => " +Path.GetFileNameWithoutExtension(video) + "-trans.mkv");
 				}
 				Console.WriteLine("");
 				if (Prompt.GetYesNo("Look Good?", true))
@@ -145,31 +150,31 @@ namespace Transcoder
 
 		}
 
-		private static async Task DownloadM3U8(string url, string outputPath)
-		{
-			var _url = new Uri(url);
+		//private static async Task DownloadM3U8(string url, string outputPath)
+		//{
+		//	var _url = new Uri(url);
 
-			IMediaInfo mediaInfo = await FFmpeg.GetMediaInfo(url);
+		//	IMediaInfo mediaInfo = await FFmpeg.GetMediaInfo(url);
 
-			//var conversion = FFmpeg.Conversions.New();
-			var conversion = await FFmpeg.Conversions.FromSnippet.SaveM3U8Stream(_url, outputPath);
-			conversion
-			//	.AddStream(mediaInfo.VideoStreams.FirstOrDefault().CopyStream())
-			//	.AddStream(mediaInfo.AudioStreams.FirstOrDefault().CopyStream())
-			//	.AddStream(mediaInfo.SubtitleStreams)
-				.SetOverwriteOutput(true)
-			;
+		//	//var conversion = FFmpeg.Conversions.New();
+		//	var conversion = await FFmpeg.Conversions.FromSnippet.SaveM3U8Stream(_url, outputPath);
+		//	conversion
+		//	//	.AddStream(mediaInfo.VideoStreams.FirstOrDefault().CopyStream())
+		//	//	.AddStream(mediaInfo.AudioStreams.FirstOrDefault().CopyStream())
+		//	//	.AddStream(mediaInfo.SubtitleStreams)
+		//		.SetOverwriteOutput(true)
+		//	;
 
-			var p = new ProgressBar(PbStyle.SingleLine, 100);
-
-
-			conversion.OnProgress += (sender, args) =>
-			{
-				p.Refresh(args.Percent, Path.GetFileNameWithoutExtension(outputPath) + $" - [{ args.TotalLength - args.Duration}]");
-			};
+		//	var p = new ProgressBar(PbStyle.SingleLine, 100);
 
 
-			await conversion.Start();
-		}
+		//	conversion.OnProgress += (sender, args) =>
+		//	{
+		//		p.Refresh(args.Percent, Path.GetFileNameWithoutExtension(outputPath) + $" - [{ args.TotalLength - args.Duration}]");
+		//	};
+
+
+		//	await conversion.Start();
+		//}
 	}
 }
